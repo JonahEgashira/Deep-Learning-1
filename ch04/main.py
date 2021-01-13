61f1516a6ac5e084c6bb55384febe47075dd3660
Jonah Egashira <jonahega@gmail.com>
2021-01-13 19:23:32 +0900

    commit on master

--- ch04/main.py
+++ ch04/main.py
@@ -1,4 +1,7 @@
-import numpy as np
-import matplotlib.pylab as plt
+from two_layer_net import TwoLayerNet
 
-x = np.arrange(0,d)
+net = TwoLayerNet(input_size = 784, hidden_size = 100, output_size = 10)
+print(net.params['W1'].shape)
+print(net.params['b1'].shape)
+print(net.params['W2'].shape)
+print(net.params['b2'].shape)
