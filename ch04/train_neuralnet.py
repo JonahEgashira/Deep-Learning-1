58a0568fe8d35464e57c5fbd69e24bbab3fbd6b1
Jonah Egashira <jonahega@gmail.com>
2021-01-14 11:50:12 +0900

    commit on master

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -13,6 +13,10 @@ batch_size = 100
 learning_rate = 0.1
 
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(iters_num):
     # ミニバッチの取得
@@ -24,10 +28,29 @@ for i in range(iters_num):
     #grad = network.numerical_gradient(x_batch, t_batch)
     grad = network.gradient(x_batch, t_batch)
 
-    for key in ('Wch1', 'b1', 'W2', 'b2'):
+    for key in ('W1', 'b1', 'W2', 'b2'):
         network.params[key] -= learning_rate * grad[key]
     
     #学習経過の記録
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
 
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+
+#グラフ描画
+
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
